74ecb6b80b1e2ee0fd116be0bb57c8941277d714
Daniel <130054513+Daniel-Astaptscev@users.noreply.github.com>
2024-12-03 21:40:30 +0400

    Add files via upload

--- assets/data/sql_requests.py
+++ assets/data/sql_requests.py
@@ -3,30 +3,46 @@ import sqlite3
 
 class Request:
     """
-        About
-        ----------
-        Основное применение - выполнение любых запросов к базе данных.
+    Основное применение - выполнение любых запросов к базе данных.
 
-        Attributes
-        ----------
-        name - имя для экземпляра класса, которое является отображением одного из типов запросов в sql
+    Args:
+        name (str): имя для экземпляра класса, которое является отображением
+        одного из типов запросов в sql.
 
-        Methods
-        -------
-        connection_with_request()
-            основной метод класса - который представляет из себя подключение к базе данных, а затем исходя из атрибута его экземпляра осуществляет тот или иной запрос (при необходимости возвращая значения из базы данных), после окончания работы производит закрытие базы данных 
+    Methods:
+        connection_with_request():
+            основной метод класса - который представляет из себя
+            подключение к базе данных, а затем исходя из атрибута его
+            экземпляра осуществляет тот или иной запрос (при необходимости
+            возвращая значения из базы данных), после окончания работы
+            производит закрытие базы данных.
     """
 
     def __init__(self, name):
         self.name = name
 
-    def connection_with_request(self, book_author=None, book_title=None,
-                                book_year=None, book_status=None,
-                                book_id=None, book_select=None):
+    def connection_with_request(self, book_author: str = None, book_title: str
+    = None, book_year: int = None, book_status: int = None,
+                                book_id: int = None) -> [None, list]:
         """
+        Создание модального окна верхнего уровня при взаимодействии
+        пользователя с инструментами основного меню.
+            
+        Args:
+            book_author (str, optional): столбец для автора книги.
 
-            Parameters
-            ----------
+            book_title (str, optional): столбец для названия книги.
+
+            book_year (int, optional): столбец для года издания книги.
+
+            book_status (int, optional): столбец для указания статуса книги.
+
+            book_id (int, optional): столбец для уникального
+            идентификационного номера.
+
+        Returns:
+            books (list, optional): формируется список из кортежей/а в
+            каждом из которых вся информация по книге/ам
         """
         connection = sqlite3.connect('./assets/data/library.db')
         cursor = connection.cursor()
@@ -42,7 +58,7 @@ class Request:
                 (book_author, book_title, book_year, book_status, book_id))
 
         elif self.name == 'DELETE':
-            cursor.execute(f'DELETE FROM Books WHERE id = {book_select}')
+            cursor.execute(f'DELETE FROM Books WHERE id = {book_id}')
 
         elif self.name == 'WHERE':
             cursor.execute(
